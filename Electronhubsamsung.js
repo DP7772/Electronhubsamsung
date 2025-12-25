98f72f394702d2d5bb0bcb241ca2275a408651b3
akash__dp <117759810+DP7772@users.noreply.github.com>
2025-12-25 18:09:03 +0530

    Add files via upload

---
+++ Electronhubsamsung.js
@@ -0,0 +1,648 @@
+<script>
+
+function Day()
+{
+var a = document.getElementById("body");
+
+var b = document.getElementById("p1");
+
+var c = document.getElementById("w1");
+
+var d = document.getElementById("buy1");
+
+var e = document.getElementById("buy2");
+
+var f = document.getElementById("buy3");
+
+var g = document.getElementById("buy4");
+
+var h = document.getElementById("buy5");
+
+var i = document.getElementById("buy6");
+
+var j = document.getElementById("buy7");
+
+var k = document.getElementById("buy8");
+
+var l = document.getElementById("buy9");
+
+var m = document.getElementById("buy10");
+
+var n = document.getElementById("buy11");
+
+var o = document.getElementById("buy12");
+
+var p = document.getElementById("buy13");
+
+var q = document.getElementById("buy14");
+
+var r = document.getElementById("buy15");
+
+var s = document.getElementById("buy16");
+
+var t = document.getElementById("buy17");
+
+var u = document.getElementById("buy18");
+
+var v = document.getElementById("buy19");
+
+var w = document.getElementById("buy20");
+
+var x = document.getElementById("t1");
+
+var y = document.getElementById("t2");
+
+var z = document.getElementById("family");
+
+var A = document.getElementById("r1");
+
+var B = document.getElementById("mic1");
+
+var C = document.getElementById("a1");
+
+var D = document.getElementById("s24");
+
+var E = document.getElementById("s23");
+
+var F = document.getElementById("s21");
+
+var G = document.getElementById("f15");
+
+var H = document.getElementById("f55");
+
+var I = document.getElementById("m14");
+
+var J = document.getElementById("fit3");
+
+var K = document.getElementById("wat6");
+
+var L = document.getElementById("snine");
+
+var M = document.getElementById("anine");
+
+var N = document.getElementById("cry");
+
+var O = document.getElementById("neo");
+
+var P = document.getElementById("wash1");
+
+var Q = document.getElementById("wash2");
+
+var R = document.getElementById("refri1");
+
+var S = document.getElementById("refri2");
+
+var T = document.getElementById("microw1");
+
+var U = document.getElementById("microw2");
+
+var V = document.getElementById("airc1");
+
+var W = document.getElementById("airc2");
+
+a.style.backgroundColor="white";
+a.style.color="black";
+b.style.backgroundColor="white";
+b.style.color="black";
+c.style.backgroundColor="white";
+c.style.color="black";
+d.style.backgroundColor="black";
+d.style.color="white";
+e.style.backgroundColor="black";
+e.style.color="white";
+f.style.backgroundColor="black";
+f.style.color="white";
+g.style.backgroundColor="black";
+g.style.color="white";
+h.style.backgroundColor="black";
+h.style.color="white";
+i.style.backgroundColor="black";
+i.style.color="white";
+j.style.backgroundColor="black";
+j.style.color="white";
+k.style.backgroundColor="black";
+k.style.color="white";
+l.style.backgroundColor="black";
+l.style.color="white";
+m.style.backgroundColor="black";
+m.style.color="white";
+n.style.backgroundColor="black";
+n.style.color="white";
+o.style.backgroundColor="black";
+o.style.color="white";
+p.style.backgroundColor="black";
+p.style.color="white";
+q.style.backgroundColor="black";
+q.style.color="white";
+r.style.backgroundColor="black";
+r.style.color="white";
+s.style.backgroundColor="black";
+s.style.color="white";
+t.style.backgroundColor="black";
+t.style.color="white";
+u.style.backgroundColor="black";
+u.style.color="white";
+v.style.backgroundColor="black";
+v.style.color="white";
+w.style.backgroundColor="black";
+w.style.color="white";
+x.style.backgroundColor="white";
+x.style.color="black";
+y.style.backgroundColor="white";
+y.style.color="black";
+z.style.backgroundColor="white";
+z.style.color="black";
+A.style.backgroundColor="white";
+A.style.color="black";
+B.style.backgroundColor="white";
+B.style.color="black";
+C.style.backgroundColor="white";
+C.style.color="black";
+D.style.backgroundColor="black";
+D.style.color="white";
+E.style.backgroundColor="black";
+E.style.color="white";
+F.style.backgroundColor="black";
+F.style.color="white";
+G.style.backgroundColor="black";
+G.style.color="white";
+H.style.backgroundColor="black";
+H.style.color="white";
+I.style.backgroundColor="black";
+I.style.color="white";
+J.style.backgroundColor="black";
+J.style.color="white";
+K.style.backgroundColor="black";
+K.style.color="white";
+L.style.backgroundColor="black";
+L.style.color="white";
+M.style.backgroundColor="black";
+M.style.color="white";
+N.style.backgroundColor="black";
+N.style.color="white";
+O.style.backgroundColor="black";
+O.style.color="white";
+P.style.backgroundColor="black";
+P.style.color="white";
+Q.style.backgroundColor="black";
+Q.style.color="white";
+R.style.backgroundColor="black";
+R.style.color="white";
+S.style.backgroundColor="black";
+S.style.color="white";
+T.style.backgroundColor="black";
+T.style.color="white";
+U.style.backgroundColor="black";
+U.style.color="white";
+V.style.backgroundColor="black";
+V.style.color="white";
+W.style.backgroundColor="black";
+W.style.color="white";
+
+
+
+
+}
+
+
+
+
+function Night()
+{
+var a = document.getElementById("body");
+
+var b = document.getElementById("p1");
+
+var c = document.getElementById("w1");
+
+var d = document.getElementById("buy1");
+
+var e = document.getElementById("buy2");
+
+var f = document.getElementById("buy3");
+
+var g = document.getElementById("buy4");
+
+var h = document.getElementById("buy5");
+
+var i = document.getElementById("buy6");
+
+var j = document.getElementById("buy7");
+
+var k = document.getElementById("buy8");
+
+var l = document.getElementById("buy9");
+
+var m = document.getElementById("buy10");
+
+var n = document.getElementById("buy11");
+
+var o = document.getElementById("buy12");
+
+var p = document.getElementById("buy13");
+
+var q = document.getElementById("buy14");
+
+var r = document.getElementById("buy15");
+
+var s = document.getElementById("buy16");
+
+var t = document.getElementById("buy17");
+
+var u = document.getElementById("buy18");
+
+var v = document.getElementById("buy19");
+
+var w = document.getElementById("buy20");
+
+var x = document.getElementById("t1");
+
+var y = document.getElementById("t2");
+
+var z = document.getElementById("family");
+
+var A = document.getElementById("r1");
+
+var B = document.getElementById("mic1");
+
+var C = document.getElementById("a1");
+
+var D = document.getElementById("s24");
+
+var E = document.getElementById("s23");
+
+var F = document.getElementById("s21");
+
+var G = document.getElementById("f15");
+
+var H = document.getElementById("f55");
+
+var I = document.getElementById("m14");
+
+var J = document.getElementById("fit3");
+
+var K = document.getElementById("wat6");
+
+var L = document.getElementById("snine");
+
+var M = document.getElementById("anine");
+
+var N = document.getElementById("cry");
+
+var O = document.getElementById("neo");
+
+var P = document.getElementById("wash1");
+
+var Q = document.getElementById("wash2");
+
+var R = document.getElementById("refri1");
+
+var S = document.getElementById("refri2");
+
+var T = document.getElementById("microw1");
+
+var U = document.getElementById("microw2");
+
+var V = document.getElementById("airc1");
+
+var W = document.getElementById("airc2");
+
+a.style.backgroundColor="black";
+a.style.color="white";
+b.style.backgroundColor="black";
+b.style.color="white";
+c.style.backgroundColor="black";
+c.style.color="white";
+d.style.backgroundColor="white";
+d.style.color="black";
+e.style.backgroundColor="white";
+e.style.color="black";
+f.style.backgroundColor="white";
+f.style.color="black";
+g.style.backgroundColor="white";
+g.style.color="black";
+h.style.backgroundColor="white";
+h.style.color="black";
+i.style.backgroundColor="white";
+i.style.color="black";
+j.style.backgroundColor="white";
+j.style.color="black";
+k.style.backgroundColor="white";
+k.style.color="black";
+l.style.backgroundColor="white";
+l.style.color="black";
+m.style.backgroundColor="white";
+m.style.color="black";
+n.style.backgroundColor="white";
+n.style.color="black";
+o.style.backgroundColor="white";
+o.style.color="black";
+p.style.backgroundColor="white";
+p.style.color="black";
+q.style.backgroundColor="white";
+q.style.color="black";
+r.style.backgroundColor="white";
+r.style.color="black";
+s.style.backgroundColor="white";
+s.style.color="black";
+t.style.backgroundColor="white";
+t.style.color="black";
+u.style.backgroundColor="white";
+u.style.color="black";
+v.style.backgroundColor="white";
+v.style.color="black";
+w.style.backgroundColor="white";
+w.style.color="black";
+x.style.backgroundColor="black";
+x.style.color="white";
+y.style.backgroundColor="black";
+y.style.color="white";
+z.style.backgroundColor="black";
+z.style.color="white";
+A.style.backgroundColor="black";
+A.style.color="white";
+B.style.backgroundColor="black";
+B.style.color="white";
+C.style.backgroundColor="black";
+C.style.color="white";
+D.style.backgroundColor="white";
+D.style.color="black";
+E.style.backgroundColor="white";
+E.style.color="black";
+F.style.backgroundColor="white";
+F.style.color="black";
+G.style.backgroundColor="white";
+G.style.color="black";
+H.style.backgroundColor="white";
+H.style.color="black";
+I.style.backgroundColor="white";
+I.style.color="black";
+J.style.backgroundColor="white";
+J.style.color="black";
+K.style.backgroundColor="white";
+K.style.color="black";
+L.style.backgroundColor="white";
+L.style.color="black";
+M.style.backgroundColor="white";
+M.style.color="black";
+N.style.backgroundColor="white";
+N.style.color="black";
+O.style.backgroundColor="white";
+O.style.color="black";
+P.style.backgroundColor="white";
+P.style.color="black";
+Q.style.backgroundColor="white";
+Q.style.color="black";
+R.style.backgroundColor="white";
+R.style.color="black";
+S.style.backgroundColor="white";
+S.style.color="black";
+T.style.backgroundColor="white";
+T.style.color="black";
+U.style.backgroundColor="white";
+U.style.color="black";
+V.style.backgroundColor="white";
+V.style.color="black";
+W.style.backgroundColor="white";
+W.style.color="black";
+
+}
+
+ 
+function family()
+{
+var a = document.getElementById("family").value;
+
+var b = document.getElementById("body");
+
+var c = document.getElementById("family");
+
+var d = document.getElementById("buy1");
+
+var e = document.getElementById("buy2");
+
+var f = document.getElementById("buy3");
+
+var g = document.getElementById("buy4");
+
+var h = document.getElementById("buy5");
+
+var i = document.getElementById("buy6");
+
+var j = document.getElementById("buy7");
+
+var k = document.getElementById("buy8");
+
+var l = document.getElementById("buy9");
+
+var m = document.getElementById("buy10");
+
+var n = document.getElementById("buy11");
+
+var o = document.getElementById("buy12");
+
+var p = document.getElementById("buy13");
+
+var q = document.getElementById("buy14");
+
+var r = document.getElementById("buy15");
+
+var s = document.getElementById("buy16");
+
+var t = document.getElementById("buy17");
+
+var u = document.getElementById("buy18");
+
+var v = document.getElementById("buy19");
+
+var w = document.getElementById("buy20");
+
+var D = document.getElementById("s24");
+
+var E = document.getElementById("s23");
+
+var F = document.getElementById("s21");
+
+var G = document.getElementById("f15");
+
+var H = document.getElementById("f55");
+
+var I = document.getElementById("m14");
+
+var J = document.getElementById("fit3");
+
+var K = document.getElementById("wat6");
+
+var L = document.getElementById("snine");
+
+var M = document.getElementById("anine");
+
+var N = document.getElementById("cry");
+
+var O = document.getElementById("neo");
+
+var P = document.getElementById("wash1");
+
+var Q = document.getElementById("wash2");
+
+var R = document.getElementById("refri1");
+
+var S = document.getElementById("refri2");
+
+var T = document.getElementById("microw1");
+
+var U = document.getElementById("microw2");
+
+var V = document.getElementById("airc1");
+
+var W = document.getElementById("airc2"); 
+
+b.style.fontFamily =a;
+c.style.fontFamily =a;
+d.style.fontFamily =a;
+e.style.fontFamily =a;
+f.style.fontFamily =a;
+g.style.fontFamily =a;
+h.style.fontFamily =a;
+i.style.fontFamily =a;
+j.style.fontFamily =a;
+k.style.fontFamily =a;
+l.style.fontFamily =a;
+m.style.fontFamily =a;
+n.style.fontFamily =a;
+o.style.fontFamily =a;
+p.style.fontFamily =a;
+q.style.fontFamily =a;
+r.style.fontFamily =a;
+s.style.fontFamily =a;
+t.style.fontFamily =a;
+u.style.fontFamily =a;
+v.style.fontFamily =a;
+w.style.fontFamily =a;
+D.style.fontFamily =a;
+E.style.fontFamily =a;
+F.style.fontFamily =a;
+G.style.fontFamily =a;
+H.style.fontFamily =a;
+I.style.fontFamily =a;
+J.style.fontFamily =a;
+K.style.fontFamily =a;
+L.style.fontFamily =a;
+M.style.fontFamily =a;
+N.style.fontFamily =a;
+O.style.fontFamily =a;
+P.style.fontFamily =a;
+Q.style.fontFamily =a;
+R.style.fontFamily =a;
+S.style.fontFamily =a;
+T.style.fontFamily =a;
+U.style.fontFamily =a;
+V.style.fontFamily =a;
+W.style.fontFamily =a;
+b.style.filter="saturate("+a+"%)";
+    
+}
+
+
+function buy1()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID S2413U99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy2()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID S2369G99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy3()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID S2139F99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy4()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID F1515G99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy5()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID F5529G99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy6()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID M1412G49               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy7()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID F3499W99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy8()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID S6369W99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy9()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID S9549F99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy10()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID A9249L99               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy11()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID C3649T90               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy12()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID N1874T90               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy13()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID A2400W00               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy14()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID A4701W20               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy15()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID R5328F12               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy16()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID RF268F22               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy17()
+{
+window.alert("Our Team Will Notify You Soon");
+}
+
+function buy18()
+{
+window.alert("Our Team Will Notify You Soon");
+}
+
+function buy19()
+{
+window.alert("Your Order Is Confirmed ✅  "+"Order ID A2811C11               "+"Get Ready With Cash To Receive Your Order.");
+}
+
+function buy20()
+{
+window.alert("Our Team Will Notify You Soon");
+}
+
+
+</script>
